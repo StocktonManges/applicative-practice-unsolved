0536d14295e5f96a60c06b28c35f3af9bcce6b2b
StocktonManges <stocktonmanges@gmail.com>
2023-06-05 07:16:44 -0600

    Completed e7.js

--- exercises/e7.js
+++ exercises/e7.js
@@ -6,6 +6,13 @@ import { data } from "../data/data";
 
 export function getPlanetsNamesWithMoons(data) {
   // Your code goes here...
+  let arr = data.planets.filter((planet) => {
+    return planet.moons != undefined;
+  })
+  .map((planet) => {
+    return planet.name;
+  });
+  return arr;
 }
 
 
