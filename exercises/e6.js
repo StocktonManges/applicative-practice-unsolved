1c6de16ba5061682587e927f6374ffff03f365d7
StocktonManges <stocktonmanges@gmail.com>
2023-06-05 07:14:35 -0600

    Completed e6.js

--- exercises/e6.js
+++ exercises/e6.js
@@ -6,6 +6,13 @@ import { data } from "../data/data";
 
 export function getAsteroidsDiscoveredAfterYear(data, year) {
   // Your code goes here...
+  let arr = data.asteroids.filter((asteroid) => {
+    return asteroid.discoveryYear > year;
+  })
+  .map((asteroid) => {
+    return asteroid.name;
+  });
+  return arr;
 }
 
 
